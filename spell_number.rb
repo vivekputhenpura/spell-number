5e4423953c596b5b69d4faf72d3d1cf75ad132ae
Vivek Babu <vivekputhenpura@gmail.com>
2020-03-18 21:51:14 +0530

    Removed regex operation. Final solution using array operations

--- spell_number.rb
+++ spell_number.rb
@@ -1,10 +1,11 @@
 # Converts 10 digit number to words
 class SpellNumbers
-    # Public method to call the library methods
-    # Accepts 10 digit number.
+    # Public method to call the library method
+    # Accepts 10 digit number
     # @param [String] number
-    # @return [Array]
-    def to_words(number)        
+    # @return [Array] on success
+    # @return [Object] on failure
+    def to_words(number)
         words = convert(number) # call the original convert method
 
         return words
@@ -12,58 +13,62 @@ class SpellNumbers
 
     private
 
-    # @method conver does the actual conversion
+    # @method Convert does the actual conversion
     # @param [String] number
-    # @return [Array]
+    # @return [Array] on success
+    # @return [Object] on failure
     def convert(number)
         # validate the parameter
         return validate(number) if validate(number) != true
 
+        combinations = []
+        $array_map = {}
+
         # read the dictionary file contents to a global variable to use in different functions
         file_name = "dictionary.txt"
         file_path = File.join(File.dirname(__FILE__), file_name)
-        $dictionary = File.read(file_path).split        
-            
-        combinationmodel = [
-            [3, 3, 4],
-            [3, 4, 3],
-            [3, 7],
-            [4, 3, 3],
-            [4, 6],
-            [5, 5],
-            [6, 4],
-            [7, 3]
-          ]
-        combinations = get_words(number)
-        # loop though the chunks and get the words for each chunk
-        for model in combinationmodel
-            comb_array = []
+        $dictionary = File.read(file_path).split
+
+        # get the alphabetic characters corresponding to the given number
+        char_map = get_chars_for_number(number)
+
+        combinationmodel = get_combination_model()
+
+        combinationmodel.each do |model|
+            word_array = []
             start = 0;
-            for count in model
-                split = number[start,count]
-                words = get_words(split)
+            
+            # split the whole number in to chunks using the model
+            # each chunk have minimum 3 digits so that the words are at least 3 characters long
+            # loop though the chunks and get the words for each chunk
+            model.each_with_index do |count, index|
+                split = number[start, count]
+                char_array = char_map[start, count] # corresponding characters for the split number
                 
+                words = get_words(split, char_array)
+
+                # if any chunk is not returning words from the dictionary, leave the combination
                 if words.nil?
-                    comb_array = []
+                    word_array = []
                     break
                 end
                 
-                print words,comb_array,"\n.....\n"
-                
-                comb_array << words
+                # the array of words for the split numbers
+                word_array << words
                 start = start + count
             end
-
+            
             # create different combination of words from the array of words for each chunk
-            if comb_array.length > 0
-                tempCombinations = get_word_combinations(comb_array)        
-                combinations = combinations + tempCombinations            
+            if word_array.length > 0
+              tempCombinations = get_word_combinations(word_array)
+              combinations = combinations + tempCombinations
             end
-        end    
-        return combinations
+        end
+
+        return combinations # final output
     end
 
-    # Validate the input param
+    # Validate the input parameter
     # @param [String] number
     # @return [Boolean] true if valid number
     # @return [Object] if invalid number
@@ -84,9 +89,11 @@ class SpellNumbers
         return true
     end
 
-    def get_words(number)
-        #number to letters mapping
-        letters = {
+    # Returns the corresponding characters for given number
+    # @param [String] number
+    # @return [Array[Array]] like [['A', 'B', 'C'], ['M', 'N', 'O']] 
+    def get_chars_for_number(number)
+        characters = {
           '2' => ['A', 'B', 'C'],
           '3' => ['D', 'E', 'F'],
           '4' => ['G', 'H', 'I'],
@@ -97,35 +104,52 @@ class SpellNumbers
           '9' => ['W', 'X', 'Y', 'Z'],
         }
         
-    
-        pattern = number.chars.map{|digit|"("+letters[digit].join("|")+")"}
-        pattern = pattern.join("")
-        pattern = /^#{pattern}$/
+        return number.chars.map{|digit|characters[digit]}
+    end
 
-        print pattern,"\n.....\n"
+    # Returns the array of combinations of number of digits by which the whole number can be split in to. Each chunk should be having at least 3 digits.
+    def get_combination_model
+        return [
+            [10],
+            [3, 3, 4],
+            [3, 4, 3],
+            [3, 7],
+            [4, 3, 3],
+            [4, 6],
+            [5, 5],
+            [6, 4],
+            [7, 3]
+        ];
+    end
+
+    # Get the words from the dictionary
+    # @param [String] number
+    # @param [Array] char_array array of characters for the number
+    # @return [Array] array of words
+    def get_words(number, char_array)
+        # check if the words are already found before
+        if !$array_map[number].nil?
+          return $array_map[number]
+        end
         
-        words = $dictionary.grep(pattern)
+        # if no words already found for the number, 
+        # generate all possible words using the array of characters for each digit
+        possible_words = char_array.first.product(*char_array[1..-1]).map(&:join)
+        words = possible_words & $dictionary # lookup the generated words in the dictionary
+        $array_map[number] = words # store in a global variable to use next time
+        
         return words
     end
 
-    def get_word_combinations(array)    
+    # Combine the array of words to populate different combination of words
+    # @param [Array] array two dimentional array of words for the chunks in a number
+    # @retun [Array]
+    def get_word_combinations(array)
         words = []
-        word = ''
-        for first in array[0]
-          word = first      
-          for second in array[1]        
-            word2 = word + ", " + second
-            if array[2]
-              for third in array[2]            
-                word3 = word2 + ", " + third            
-                words << word3
-              end
-            else
-              words << word2
-            end
-          end
+        array.first.product(*array[1..-1]).each do |word|
+          words << word.join(",")
         end
+    
         return words
     end
 end
-print SpellNumbers.new.to_words('6686787825')
