6123c708da64d564447e71e991bbe83895ed427f
vivekputhenpura <47312055+vivekputhenpura@users.noreply.github.com>
2020-03-18 19:05:34 +0530

    Finding all the matching words
    the number is split into chunks and finding all matching words using regular expression

--- spell_number.rb
+++ spell_number.rb
@@ -19,39 +19,44 @@ class SpellNumbers
         # validate the parameter
         return validate(number) if validate(number) != true
 
-        
+        # read the dictionary file contents to a global variable to use in different functions
         file_name = "dictionary.txt"
         file_path = File.join(File.dirname(__FILE__), file_name)
-        $dictionary = File.read(file_path).split
-
-        characters = {
-            '2' => ['A', 'B', 'C'],
-            '3' => ['D', 'E', 'F'],
-            '4' => ['G', 'H', 'I'],
-            '5' => ['J', 'K', 'L'],
-            '6' => ['M', 'N', 'O'],
-            '7' => ['P', 'Q', 'R', 'S'],
-            '8' => ['T', 'U', 'V'],
-            '9' => ['W', 'X', 'Y', 'Z'],
-        }
+        $dictionary = File.read(file_path).split        
             
-        # char_map = number.chars.map{|digit|characters[digit]}
-        
-        pattern = number.chars.map{|digit|"("+characters[digit].join("|")+")"}
-        pattern = pattern.join("")
-        pattern = /^#{pattern}$/
-        
-        words = $dictionary.grep(pattern)
-    
-        pattern = number[0,3].chars.map{|digit|"("+characters[digit].join("|")+")"}
-        pattern = pattern.join("")
-        pattern = /^#{pattern}$/           
-        
-        words2 = $dictionary.grep(pattern)
+        combinationmodel = [
+            [3, 3, 4],
+            [3, 4, 3],
+            [3, 7],
+            [4, 3, 3],
+            [4, 6],
+            [5, 5],
+            [6, 4],
+            [7, 3]
+          ]
+        combinations = get_words(number)
 
-        comb = words + words2
+        for model in combinationmodel
+            comb_array = []
+            start = 0;
+            for count in model
+                split = number[start,count]
+                words = get_words(split)
+                
+                if words.nil?
+                    comb_array = []
+                    break
+                end
+                
+                print words,comb_array,"\n.....\n"
+                
+                comb_array << words
+                start = start + count
+            end
 
-        return comb
+            comb_array
+        end    
+        return comb_array
     end
 
     # Validate the input param
@@ -73,6 +78,30 @@ class SpellNumbers
                         }.length > 0
 
         return true
+    end
+
+    def get_words(number)
+        #number to letters mapping
+        letters = {
+          '2' => ['A', 'B', 'C'],
+          '3' => ['D', 'E', 'F'],
+          '4' => ['G', 'H', 'I'],
+          '5' => ['J', 'K', 'L'],
+          '6' => ['M', 'N', 'O'],
+          '7' => ['P', 'Q', 'R', 'S'],
+          '8' => ['T', 'U', 'V'],
+          '9' => ['W', 'X', 'Y', 'Z'],
+        }
+        
+    
+        pattern = number.chars.map{|digit|"("+letters[digit].join("|")+")"}
+        pattern = pattern.join("")
+        pattern = /^#{pattern}$/
+
+        print pattern,"\n.....\n"
+        
+        words = $dictionary.grep(pattern)
+        return words
     end
 end
 print SpellNumbers.new.to_words('6686787825')
